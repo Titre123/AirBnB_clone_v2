b5d5994bb327cc5570cf7ba0d8cc48f9ef5e88f9
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-10-02 18:48:54 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,21 +5,21 @@ if [[ ! `dpkg --get-selections | grep nginx` ]]; then
         sudo apt -y update
         sudo apt -y install nginx
 fi
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 test -e /data/web_static/releases/test/index.html || sudo touch /data/web_static/releases/test/index.html
 dir="/data/web_static/releases/test/index.html"
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
   </html>" > $dir
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-sudo service nginx  restart
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+service nginx  restart
 exit 0
