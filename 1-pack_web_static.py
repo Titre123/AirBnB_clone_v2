35aed3f10c1f269dadcda9c27d17e5eda89189ce
Ola-balogun Taiwo <taiwotriumphant@gmail.com>
2022-10-03 01:20:43 +0000

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,11 +4,10 @@ from fabric.operations import *
 from datetime import datetime
 date = datetime.now().strftime("%Y%m%d%H%M%S")
 def do_pack():
-	'''fab fucntion to compress web_static files'''
-	local('mkdir -p versions')
-	re = local('tar -cvzf versions/web_static_{} web_static'
-		   .format(date), capture=True)
-	
-	if re.failed:
-		return None
-	return re
+    '''fab fucntion to compress web_static files'''
+    local('mkdir -p versions')
+    re = local('tar -cvzf versions/web_static_{}.tgz ~/AirBnB_clone_v2/web_static'
+	       .format(date), capture=True)
+    if re.failed:
+        return None
+    return re
