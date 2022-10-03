241faf7f30fa4a256b8fcd3ea411b500c58024a6
Taiwo Ola-Balogun <taiwotriumphant@gmail.com>
2022-10-02 17:39:40 -0700

    new

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,14 @@
-from fabric.api import *
+#!/usr/bin/python3
+# compress web_static files
+from fabric.operations import *
+from datetime import datetime
+date = datetime.now().strftime("%y%m%d%H%M%S")
+def do_pack():
+	'''fab fucntion to compress web_static files'''
+	local('mkdir -p versions')
+	file = f"versions/web_static_{date}.tgz"
+	re = local(f'tar -cvzf {file} ~/AirBnB_clone_v2/web_static', capture=True)
+	
+	if re.failed:
+		return None
+	return re
